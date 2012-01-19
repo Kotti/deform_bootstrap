9a26464c5c31627735802765a0aceed1fb409e61
Daniel Nouri <daniel.nouri@gmail.com>
2012-01-19 18:47:45 +0100

    Increment version number.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 setup(
     name='deform_bootstrap',
-    version='0.1a1',
+    version='0.1a2',
     description="Bootstrap compatible templates for the deform form library",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
