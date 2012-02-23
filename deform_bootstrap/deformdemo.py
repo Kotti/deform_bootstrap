9aa7b1ae1f1685517b074dbea59419d8d1a769f1
Andreas Kaiser <disko@binary-punks.com>
2012-02-23 11:03:23 +0100

    - fixed broken import in deformdemo.py - changed master macro in deform-main.pt to be real HTML5

--- deform_bootstrap/deformdemo.py
+++ deform_bootstrap/deformdemo.py
@@ -1,13 +1,18 @@
 from deform import ZPTRendererFactory
+from pkg_resources import resource_filename
 from pyramid.i18n import get_localizer
 from pyramid.threadlocal import get_current_request
 
 from deform_bootstrap import includeme as base_includeme
-from deform_bootstrap import search_path
 
 # Code here exists solely to allow the running of deformdemo.  Use the
 # 'includeme' from the __init__.py or similar in your production code,
 # not code from here:
+
+search_path = (
+        resource_filename('deform_bootstrap', 'templates'),
+        resource_filename('deform', 'templates'),
+        )
 
 def translator(term):
     return get_localizer(get_current_request()).translate(term)
