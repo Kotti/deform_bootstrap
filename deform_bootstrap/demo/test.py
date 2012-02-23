21905e9980c6643792b719d9a78b32f2e81bffaa
Daniel Nouri <daniel.nouri@gmail.com>
2012-02-23 17:26:43 +0100

    Move demo into its own directory.
    Override one of the original Selenium tests in deformdemo.
    
    For instructions on running tests see README.rst.

---
+++ deform_bootstrap/demo/test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from deformdemo import test
+
+browser = None
+
+def patch_test_render_default(self):
+    browser.open(self.url)
+    browser.wait_for_page_to_load("30000")
+
+    self.assertTrue(browser.is_text_present(u"По оживлённым берегам"))
+    # self.assertEqual(browser.get_attribute("item-deformField1@title"),
+    #                  description)
+    # self.assertEqual(browser.get_attribute("css=label@title"),
+    #                  description)
+    self.assertEqual(browser.get_attribute("deformField1@name"), 'field')
+    self.assertEqual(browser.get_value("deformField1"), u'☃')
+    self.assertEqual(browser.get_text('css=#captured'), 'None')
+
+def _patch():
+    test.UnicodeEverywhereTests.test_render_default = patch_test_render_default
+
+if __name__ == '__main__':
+    test.setUpModule()
+    browser = test.browser
+    _patch()
+    try:
+        unittest.main(test)
+    finally:
+        test.tearDownModule()
