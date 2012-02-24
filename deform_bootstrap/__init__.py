e718d521ed2fbe74d9a4ec7d1e4688c77c793787
Tom Lazar <tom@tomster.org>
2012-02-24 12:27:08 +0100

    test our includeme. this creates 100% test coverage and also unearthed a typo :-)

--- deform_bootstrap/__init__.py
+++ deform_bootstrap/__init__.py
@@ -2,11 +2,13 @@ from pkg_resources import resource_filename
 
 from deform import Form
 
+
 def add_search_path():
     loader = Form.default_renderer.loader
     loader.search_path = (
         resource_filename('deform_bootstrap', 'templates'),
         ) + loader.search_path
+
 
 def includeme(config):
     add_search_path()
