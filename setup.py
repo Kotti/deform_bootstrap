175829c329744d30fb3a34e488453a1672704444
Daniel Nouri <daniel.nouri@gmail.com>
2012-07-11 11:15:47 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 setup(
     name='deform_bootstrap',
-    version='0.2.2',
+    version='0.2.3dev',
     description="Twitter Bootstrap compatible widgets, templates and styles for the deform form library",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
