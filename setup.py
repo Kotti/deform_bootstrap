2d078995df1b83310d21a289b36cc3eb9f8fe52f
Daniel Nouri <daniel.nouri@gmail.com>
2012-10-24 21:12:39 +0200

    0.2.4 release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requires = [
 
 setup(
     name='deform_bootstrap',
-    version='0.2.4dev',
+    version='0.2.4',
     description="Twitter Bootstrap compatible widgets, templates and styles for the deform form library",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
