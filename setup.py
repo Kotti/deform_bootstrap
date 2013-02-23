ff9165d33404c0f6a2c51bb371c3e842752ccc58
Andreas kaiser <disko@binary-punks.com>
2013-02-23 09:45:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='deform_bootstrap',
-    version='0.2.6',
+    version='0.2.7dev',
     description="Twitter Bootstrap compatible widgets, templates and styles for the deform form library",
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
