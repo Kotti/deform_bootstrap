b35180c1c565c87394752196f8808fcd6ebd17f9
Nando Florestan <nandoflorestan@gmail.com>
2013-05-07 19:16:29 -0300

    Work around broken resource_filename() on Google AppEngine

--- deform_bootstrap/__init__.py
+++ deform_bootstrap/__init__.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from pkg_resources import resource_filename
-
 from deform import Form
-
 from .resources import default_resources
+
 
 def add_resources_to_registry():
     """
@@ -15,14 +16,21 @@ def add_resources_to_registry():
             registry.set_js_resources(rqrt, version, *resources.get('js'))
             registry.set_css_resources(rqrt, version, *resources.get('css'))
 
+
 def add_search_path():
     loader = Form.default_renderer.loader
-    loader.search_path = (
-        resource_filename('deform_bootstrap', 'templates'),
-        ) + loader.search_path
+    try:
+        path = resource_filename('deform_bootstrap', 'templates')
+    except ImportError:
+        # On Google AppEngine resource_filename() uses os.path.expanduser()
+        # which uses pwd which isn't available, so we work around that mess:
+        from os.path import dirname, join
+        path = join(dirname(__file__), 'templates')
+    loader.search_path = (path,) + loader.search_path
 
 
 def includeme(config):
     add_search_path()
     add_resources_to_registry()
-    config.add_static_view('static-deform_bootstrap', 'deform_bootstrap:static')
+    config.add_static_view(
+        'static-deform_bootstrap', 'deform_bootstrap:static')
